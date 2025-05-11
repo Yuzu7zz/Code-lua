45b054662c5946d4f751f96e00a83b70883977db
Yuzu <powerpizza688@gmail.com>
2025-05-12 01:34:22 +0700

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Mobile-Shiftlock-V2-18053"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzu7zz/Code-lua/refs/heads/main/lock.lua"))()
